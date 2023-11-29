3cfb17d7a1261d0b1dc37cb6d8db5a1d03fd0df1
Felipe Rosa de Mendonça <107950074+feliperosademendonca@users.noreply.github.com>
2023-11-29 10:17:04 -0300

    editando dependencias e start

--- index.js
+++ index.js
@@ -232,6 +232,10 @@ app.get('/public/estilo.css', function(req, res) {
 
 });
 
-app.listen(3000, () => 
-console.log('Servidor iniciado na porta 3000: http://localhost:3000')
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => 
+console.log('Servidor iniciado na porta 3000: http://localhost:3000'),
+console.info('Servidor iniciado na porta 3000: http://localhost:3000'),
+
 );
