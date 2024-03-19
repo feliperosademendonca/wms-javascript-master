a12b0f5a44e69b678f41897c60f58a4c693f5bb9
Felipe Rosa de Mendonça <107950074+feliperosademendonca@users.noreply.github.com>
2024-03-19 20:35:18 -0300

    implementando sequelize e variaveis de ambiente

--- index.js
+++ index.js
@@ -9,10 +9,18 @@ const fs = require('fs');
 const path = require('path');
 const db = require("./bd/models")
 app.set('views', path.join(__dirname, 'views'));
-const bd = fs.readFileSync('bd.csv', 'utf8');
-const end = require("./end.js")
+const env = process.env.NODE_ENV || 'development';
 
+const config = require( __dirname + "/bd/config/config.js")[env];
+const Sequelize = require("sequelize");
 
+let sequelize;
+if(config.use_env_variable){
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+}else{  
+  sequelize = new Sequelize(config.database,config.username, config.password,config,);
+
+}
  
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
